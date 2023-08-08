91c688c08ad90d48deef2a8aea2e60d553697206
VictorVidal12 <victormanuelvb12@gmail.com>
2023-08-08 12:54:51 -0500

    Correcciones de PEP 8 realizadas en Ejercicio 1

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,13 +1,9 @@
+class Vehiculo:  # clase vehiculo creada
 
+    def __init__(self, speed: float, kilo: float):  # constructor creado, variables definidas
+        self.velocidad_maxima = speed  # variable velocidad maxima asignada
+        self.kilometraje = kilo  # variable kilometraje asignada
 
-class Vehiculo:
-    velocidad_maxima: float
-    kilometraje: float
-
-    def __init__(self, speed: float = input(), kilo: float = input()):
-        self.velocidad_maxima = speed
-        self.kilometraje = kilo
-
-    def accesso(self):
+    def accesso(self):  # funcion para leer la informacion guardada
         print("La informacion del vehiculo es:  " + str(self.velocidad_maxima) + " MaxSpeed ")
         print(str(self.kilometraje) + " Kilometrake")

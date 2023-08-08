d6095f818c506fb3392de3f97222b2d7dad986ff
Ospina <o.lamprea99@gmail.com>
2023-08-08 18:50:16 -0500

    Ejercicio 4 realizado

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,12 +1,24 @@
+from Ejercicio2 import Punto
+
+
 class Rectangulo:
     def __init__(self):
-        self.esquina_inferior_izquierda = [0.0, 0.0]
-        self.esquina_superior_derecha = [0.0, 0.0]
+        self.esquina_izquierda = Punto(0.0, 0.0)
+        self.esquina_derecha = Punto(0.0, 0.0)
 
-    def asignar_coordenadas(self, esquina_inferior_x: float, esquina_inferior_y: float, esquina_superior_x: float, esquina_superior_y: float):
-        self.esquina_inferior_izquierda[0] = esquina_inferior_x
-        self.esquina_inferior_izquierda[1] = esquina_inferior_y
-        self.esquina_superior_derecha[0] = esquina_superior_x
-        self.esquina_superior_derecha[1] = esquina_superior_y
+    def perimetro(self):
+        largo_en_x = abs(self.esquina_izquierda.posicion_X - self.esquina_derecha.posicion_X)
+        largo_en_y = abs(self.esquina_izquierda.posicion_y - self.esquina_derecha.posicion_y)
+        return 2 * largo_en_x + 2 * largo_en_y
 
-    
+    def area(self):
+        area = abs(self.esquina_izquierda.posicion_X - self.esquina_derecha.posicion_X) \
+               * abs(self.esquina_izquierda.posicion_y - self.esquina_derecha.posicion_y)
+        return area
+
+    def es_cuadrado(self):
+        if abs(self.esquina_izquierda.posicion_X - self.esquina_derecha.posicion_X) == \
+                abs(self.esquina_izquierda.posicion_y - self.esquina_derecha.posicion_y):
+            return True
+        else:
+            return False

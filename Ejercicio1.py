bddb8c50069a83d2cd3bb74e18630c8685c7bc3e
VictorVidal12 <victormanuelvb12@gmail.com>
2023-08-08 12:24:49 -0500

    Ejercicio1.. .gitignore and requirements

---
+++ Ejercicio1.py
@@ -0,0 +1,12 @@
+
+
+class Vehiculo:
+    velocidad_maxima: float
+    kilometraje: float
+
+    def Vehiculo(self, speed: float, kilo: float):
+        self.velocidad_maxima = speed
+        self.kilometraje = kilo
+
+
+
